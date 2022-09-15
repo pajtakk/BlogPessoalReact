cd789669d3721e35c1cbe55b3be7d81c2172059d
pajtak <fehpaj@gmail.com>
2022-09-15 20:58:28 -0300

    criando e estilizando home

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import { TokenState } from '../../store/tokens/tokens-reducer';
+
+import TabPostagem from '../../components/postagens/tabPostagem/TabPostagem';
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import ModalPostagem from '../../components/postagens/modalPostagem/modalPostagem';
+import './home.css';
+import { useSelector } from 'react-redux';
+import { Typography, Box, Grid, Button } from '@material-ui/core';
+
+
+
+function Home() {
+
+    let navigate = useNavigate();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.info('Você precisa estar logado.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+            navigate("/login")
+
+        }
+    }, [token])
+
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalPostagem />
+                        </Box>
+                        <Link to="/postagens" className='text-decorator-none'>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        </Link>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://cdn.discordapp.com/attachments/710276943592816720/1014731648832651354/sign-up-form-4575543-3798675.webp" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
