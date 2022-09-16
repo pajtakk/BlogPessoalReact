380396447b11edc69209e3a03f50490dee8adbd6
pajtak <fehpaj@gmail.com>
2022-09-15 21:06:58 -0300

    criando e estilizando componente login

---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
+
+import { Grid, TextField, Typography, Button, } from '@material-ui/core';
+import { Box } from '@mui/material';
+
+import { login } from "../../services/Service";
+import UserLogin from "../../models/UserLogin";
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToken } from '../../store/tokens/actions';
+import { toast } from 'react-toastify';
+
+import './SignIn.css';
+
+function SignIn() {
+
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [token, setToken] = useState("");
+    const [userLogin, setUserLogin] = useState<UserLogin>({
+        id: 0,
+        nome: "",
+        foto: "",
+        usuario: "",
+        senha: "",
+        token: "",
+    });
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    useEffect(() => {
+        if (token != "") {
+            dispatch(addToken(token))
+            navigate("/home");
+        }
+    }, [token]);
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            await login(`/usuarios/logar`, userLogin, setToken);
+
+            toast.success('Usuario logado com sucesso.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        } catch (error) {
+            toast.error('Dados do usuário inconsistentes.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+                
+        }
+    }
+
+    return (
+        <>
+            <Grid container>
+
+                <Box className="container">
+
+                    <Box className="signin-box">
+
+                        <Box className="signin">
+
+                            <Box className="signin-container">
+
+                                <form className="form" onSubmit={onSubmit}>
+                                    <Typography className="title" variant="h3"> Welcome</Typography>
+
+                                    <Box className="input-container">
+                                        <TextField
+                                            value={userLogin.usuario}
+                                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                            className="input"
+                                            id="username"
+                                            name="usuario"
+                                            label="Username"
+                                            type="email"
+                                            variant="filled"
+                                        />
+
+                                        <TextField
+                                            value={userLogin.senha}
+                                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                            className="input"
+                                            id="password"
+                                            name="senha"
+                                            label="Password"
+                                            type="password"
+                                            autoComplete="current-password"
+                                            variant="filled"
+                                        />
+                                    </Box>
+
+
+                                    <Box className="links-container">
+                                        <Link to='' className="links">I forgot my password</Link>
+                                        <Link to='/register' className="links">I don't have an account</Link>
+                                    </Box>
+
+                                        <Button className="button" type="submit">LOGIN</Button>
+                                    
+                                </form>
+
+                            </Box>
+
+                        </Box>
+
+                        <Box className="box-image">
+
+                            <img className="img" src="https://besthqwallpapers.com/Uploads/25-5-2020/134632/thumb2-4k-black-stone-background-cracked-stone-texture-stone-textures-grunge-backgrounds.jpg" alt="login-image" />
+
+                        </Box>
+
+                    </Box>
+
+                </Box>
+
+            </Grid>
+        </>
+    )
+}
+
+export default SignIn;
