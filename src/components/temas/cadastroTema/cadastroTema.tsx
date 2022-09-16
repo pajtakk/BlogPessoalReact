753dedfe6f38697b06e83e96aadb908a8206d4f6
pajtak <fehpaj@gmail.com>
2022-09-15 21:05:38 -0300

    criando e estilizando componentes relacionados a tema

---
+++ src/components/temas/cadastroTema/cadastroTema.tsx
@@ -0,0 +1,121 @@
+import React, {useState, useEffect, ChangeEvent} from 'react'
+import { Container, Typography, TextField, Button } from "@material-ui/core"
+import { useNavigate, useParams } from 'react-router-dom'
+import './cadastroTema.css';
+import Tema from '../../../models/Tema';
+import { buscaId, post, put } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokens-reducer';
+import { toast } from 'react-toastify';
+
+
+function CadastroTema() {
+    let navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        tema: '',
+        descricao: ''
+    })
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    useEffect(() => {
+        if (token === "") {
+            toast.info('Você precisa estar logado.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+            navigate("/login")
+    
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/temas/${id}`, setTema, {
+            headers: {
+              'Authorization': token
+            }
+          })
+        }
+
+        function updatedTema(e: ChangeEvent<HTMLInputElement>) {
+
+            setTema({
+                ...tema,
+                [e.target.name]: e.target.value,
+            })
+    
+        }
+        
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+            e.preventDefault()
+            console.log("tema " + JSON.stringify(tema))
+    
+            if (id !== undefined) {
+                console.log(tema)
+                put(`/temas`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                toast.success('Tema atualizado com sucesso.', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    });
+            } else {
+                post(`/temas`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                toast.success('Tema cadastrado com sucesso.', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    });
+            }
+            back()
+    
+        }
+    
+        function back() {
+            navigate('/temas')
+        }
+  
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
+                <TextField value={tema.tema} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="tema" label="tema" variant="outlined" name="tema" margin="normal" fullWidth />
+                <TextField value={tema.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <Button className='botfin'  type="submit" variant="contained">
+                    Finalizar
+                </Button>
+            </form>
+        </Container>
+    )
+}
+
+export default CadastroTema;
---
+++ src/components/temas/listaTema/ListaTema.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import Tema from '../../../models/Tema';
+import { useNavigate } from 'react-router-dom';
+import './ListaTema.css';
+import { busca } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokens-reducer';
+import { toast } from 'react-toastify';
+
+function ListaTema() {
+
+    const [temas, setTemas] = useState<Tema[]>([])
+    let navigate = useNavigate();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    useEffect(() => {
+        if (token == '') {
+            toast.info('Você precisa estar logado.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+            navigate("/login")
+        }
+    }, [token])
+
+    async function getTema() {
+        await busca("/temas", setTemas, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getTema()
+    }, [temas.length])
+
+    return (
+        <>
+            {
+                temas.map(tema => (
+                    <Box className='fund' >
+                    <Box className='fundocontainer'  m={2} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Tema
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {tema.tema}
+                                </Typography>
+                                <Typography>
+                                    {tema.descricao}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5} >
+
+                                    <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button className='atualizabotao' variant="contained"  size='small' color="primary" >
+                                                atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button className='deletbotao' variant="contained" size='small' color="secondary">
+                                                deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                </Box>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </Box>
+                ))
+            }
+        </>
+    );
+}
+
+
+export default ListaTema;
